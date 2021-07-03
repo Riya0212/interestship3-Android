0d288322b3a57e2151df19e711786735ad9884da
Riya <ria020601@gmail.com>
2021-07-03 14:53:01 +0530

    initial

---
+++ app/src/main/java/com/example/wecureloginactivity/login.kt
@@ -0,0 +1,14 @@
+package com.example.wecureloginactivity
+
+import androidx.appcompat.app.AppCompatActivity
+
+
+class login : AppCompatActivity(){
+    var user_name:String?=null
+    get()=field
+    set(user_name){
+        field=user_name
+
+    }
+
+}
