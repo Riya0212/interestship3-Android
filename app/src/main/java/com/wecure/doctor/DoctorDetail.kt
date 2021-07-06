84db3be056b19c496daffe375c744837d4bb4e61
Hrishita Mavani <hrishu10.hd@gmail.com>
2021-07-07 01:25:33 +0530

    doctor profile

---
+++ app/src/main/java/com/wecure/doctor/DoctorDetail.kt
@@ -0,0 +1,11 @@
+package com.wecure.doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DoctorDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_doctor_detail)
+    }
+}
