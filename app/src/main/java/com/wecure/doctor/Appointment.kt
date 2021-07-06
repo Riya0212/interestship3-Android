cc505c104d593242bcb40b18378ff8f062678d41
Hrishita Mavani <hrishu10.hd@gmail.com>
2021-07-07 03:32:47 +0530

    appointment screen

---
+++ app/src/main/java/com/wecure/doctor/Appointment.kt
@@ -0,0 +1,12 @@
+package com.wecure.doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Appointment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appointment)
+        supportActionBar?.hide()
+    }
+}
--- app/src/main/java/com/wecure/doctor/DoctorDetail.kt
+++ app/src/main/java/com/wecure/doctor/DoctorDetail.kt
@@ -1,12 +1,20 @@
 package com.wecure.doctor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DoctorDetail : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_detail)
         supportActionBar?.hide()
+        val floatButton:FloatingActionButton = findViewById(R.id.floating_appointent)
+        floatButton.setOnClickListener{
+            val intent = Intent(this, Appointment::class.java)
+            startActivity(intent)
+        }
     }
 }
