826ef273d3bfc4048990692789f79271cdd47270
Hrishita Mavani <hrishu10.hd@gmail.com>
2021-07-08 20:33:24 +0530

    Merge

--- app/src/main/java/com/wecure/doctor/ContactUs.kt
+++ app/src/main/java/com/wecure/doctor/ContactUs.kt
@@ -1,19 +1,30 @@
 package com.wecure.doctor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import com.wecure.doctor.databinding.ActivityContactUsBinding
 
 class ContactUs : AppCompatActivity() {
+    private lateinit var binding:ActivityContactUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_us)
         supportActionBar?.hide()
+        binding= ActivityContactUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val window: Window = this.window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.setStatusBarColor(this.resources.getColor(R.color.pink))
+
+        binding.btnsubmit.setOnClickListener {
+            Toast.makeText(this,"Thank You for Contactig us",Toast.LENGTH_SHORT).show()
+            val intent=Intent(this,HomeScreen::class.java)
+            startActivity(intent)
+        }
 
     }
 }
